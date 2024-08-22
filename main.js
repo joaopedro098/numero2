8eed31f50876188265fa6cbbefdd9cb73767d14c
joaopedro098 <163419020+joaopedro098@users.noreply.github.com>
2024-08-22 09:58:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let numero
+let quadrado
+for(let i=0;i>=10;i++){
+    numero= prompt("informe um numero")
+while (numero!=0) {
+    quadrado =(numero*numero)
+    document.write(`o numero ${numero} elevado ao quadeado é: ${quadrado}<br>`)
+    
+}
+}
